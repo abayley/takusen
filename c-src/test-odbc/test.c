89223433c88fe12caaa2106a887237ae62005c8c
alistair <alistair@abayley.org>
2008-03-21 15:54:06 +0000

    ODBC test.c: get datetime parameter test working.

--- c-src/test-odbc/test.c
+++ c-src/test-odbc/test.c
@@ -9,19 +9,18 @@ SQLRETURN rc;
 
 void printDiagRecs(SQLHANDLE h, SQLSMALLINT htype, SQLSMALLINT msgSeq)
 {
-  char strState[6];
+  char strState[20];
   char strMsg[1025];
   SQLINTEGER errNum;
   SQLSMALLINT msgLen;
   SQLRETURN rc2;
   rc2 = SQLGetDiagRec(htype, h, msgSeq, strState, &errNum, strMsg, 1024, &msgLen);
-  if (SQL_SUCCESS != rc2)
+  if (SQL_SUCCESS == rc2)
   {
-    strState[7] = '\0';
-  	strMsg[msgLen+1] = '\0';
     printf("%d %s %s\n", errNum, strState, strMsg);
     printDiagRecs(h, htype, msgSeq+1);
   }
+  /* else { printf("printDiagRecs: rc2 = %d\n", rc2); } */
 }
 
 void checkRc()
@@ -35,10 +34,10 @@ void checkRc()
 
 void checkError(SQLRETURN rc, SQLHANDLE h, SQLSMALLINT htype)
 {
-  printf("checkError: rc = %d\n", rc);
+  //printf("checkError: rc = %d\n", rc);
   if (! SQL_SUCCEEDED(rc))
   {
-  	printDiagRecs(h, htype, 0);
+  	printDiagRecs(h, htype, 1);
   	exit(1);
   }
 }
@@ -104,15 +103,52 @@ void bindParamDatetime(SQLHSTMT stmt, SQLUSMALLINT pos, char* val)
   checkError(rc, stmt, SQL_HANDLE_STMT);
 }
 
+void bindParamString(SQLHSTMT stmt, SQLUSMALLINT pos, char* val)
+{
+  SQLLEN inputSz;
+  rc = SQLBindParameter(stmt, pos, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_CHAR, 0, 0, val, strlen(val), &inputSz);
+  checkError(rc, stmt, SQL_HANDLE_STMT);
+}
+
+void bindParamInt(SQLHSTMT stmt, SQLUSMALLINT pos, int val)
+{
+  SQLLEN inputSz;
+  rc = SQLBindParameter(stmt, pos, SQL_PARAM_INPUT, SQL_C_LONG, SQL_INTEGER, 0, 0, &val, 4, &inputSz);
+  checkError(rc, stmt, SQL_HANDLE_STMT);
+}
+
 void executeStmt(SQLHSTMT stmt)
 {
   rc = SQLExecute(stmt);
   checkError(rc, stmt, SQL_HANDLE_STMT);
 }
 
-void bindCol ()
+void bindColString (SQLHSTMT stmt, SQLUSMALLINT pos, char* buffer, SQLLEN size, SQLLEN* outSz)
 {
+  rc = SQLBindCol(stmt, pos, SQL_CHAR, buffer, size, outSz);
+  checkError(rc, stmt, SQL_HANDLE_STMT);
 }
+
+void bindColInt (SQLHSTMT stmt, SQLUSMALLINT pos, int* buffer)
+{
+  SQLLEN outSz;
+  rc = SQLBindCol(stmt, pos, SQL_INTEGER, buffer, 4, &outSz);
+  checkError(rc, stmt, SQL_HANDLE_STMT);
+}
+
+void bindColDatetime (SQLHSTMT stmt, SQLUSMALLINT pos, char* buffer, SQLLEN* outSz)
+{
+  rc = SQLBindCol(stmt, pos, SQL_TYPE_TIMESTAMP, buffer, 50, outSz);
+  checkError(rc, stmt, SQL_HANDLE_STMT);
+}
+
+
+void fetch(SQLHSTMT stmt)
+{
+  rc = SQLFetch(stmt);
+  checkError(rc, stmt, SQL_HANDLE_STMT);
+}
+
 
 SQLHDBC createConn(char *connstr)
 {
@@ -130,17 +166,36 @@ SQLHDBC createConn(char *connstr)
   printf("allocStmt\n");
   stmt = allocStmt(conn);
   printf("prepareStmt\n");
-  prepareStmt(stmt, "select ?, ?, ?, ?");
+  prepareStmt(stmt, "select ?, ?, ?");
+  //prepareStmt(stmt, "select 'hello1', 'hello2'");
+  
   printf("bind param 1\n");
+  //bindParamString(stmt, 1, "hello1");
   bindParamDatetime(stmt, 1, "1753-01-01 00:00:00");
-  /*
   printf("bind param 2\n");
   bindParamString(stmt, 2, "hello2");
   printf("bind param 3\n");
   bindParamInt(stmt, 3, 4444);
-  */
+
   printf("executeStmt\n");
   executeStmt(stmt);
+
+  char col1Buf[2000];
+  SQLLEN col1Sz;
+  char col2Buf[2000];
+  SQLLEN col2Sz;
+  int col3Buf;
+  SQLLEN col3Sz;
+  bindColDatetime(stmt, 1, col1Buf, &col1Sz);
+  //bindColString(stmt, 1, col1Buf, 2000, &col1Sz);
+  bindColString(stmt, 2, col2Buf, 2000, &col2Sz);
+  bindColInt(stmt, 3, &col3Buf);
+
+  fetch(stmt);
+  printf("col1: %d\n", col1Buf, col1Sz);
+  printf("col2: %s %d\n", col2Buf, col2Sz);
+  printf("col3: %d %d\n", col3Buf, col3Sz);
+
   printf("freeStmt\n");
   freeStmt(stmt);
   printf("disconnect\n");
