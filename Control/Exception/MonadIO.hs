64626f9e31c460a3e331da0b51359c8d0c712885
alistair <alistair@abayley.org>
2009-02-02 16:19:49 +0000

    Control/Exception/MonadIO.hs: use new Exceptions that came with ghc-6.10.1.

--- Control/Exception/MonadIO.hs
+++ Control/Exception/MonadIO.hs
@@ -1,4 +1,6 @@
 
+{-# LANGUAGE CPP #-}
+
 module Control.Exception.MonadIO
 (
   CaughtMonadIO(..)
@@ -9,30 +11,23 @@ import Control.Monad.Trans
 import Control.Exception
 import Control.Monad.Reader
 
+
+#ifdef NEW_EXCEPTION
+gtry :: (Exception e, CaughtMonadIO m) => m b -> m (Either e b)
+gtryJust :: (Exception e, CaughtMonadIO m) => (e -> Maybe b) -> m b1 -> m (Either b b1)
+class MonadIO m => CaughtMonadIO m where
+  gcatch :: (Exception e) => m a -> (e -> m a) -> m a
+  gcatchJust :: (Exception e) => (e -> Maybe b) -> m a -> (b -> m a) -> m a
+#else
 gtry :: (CaughtMonadIO m) => m b -> m (Either Exception b)
-gtry a = gcatch (liftM Right a) (return . Left)
-
-gtryJust :: (CaughtMonadIO m) =>
-  (Exception -> Maybe b) -> m b1 -> m (Either b b1)
-gtryJust p a = gcatchJust p (liftM Right a) (return . Left)
-
-gbracket :: (CaughtMonadIO m) =>
-  m t -> (t -> m a) -> (t -> m b) -> m b
-gbracket acq rel act = do
-  r <- acq
-  gcatch (do
-      v <- act r
-      rel r
-      return v
-    )
-    (\e -> rel r >> throw e)
-
-gfinally :: (CaughtMonadIO m) => m t -> m a -> m t
-gfinally a f = gbracket (return ()) (const f) (const a)
-
+gtryJust :: (CaughtMonadIO m) => (Exception -> Maybe b) -> m b1 -> m (Either b b1)
 class MonadIO m => CaughtMonadIO m where
   gcatch :: m a -> (Exception -> m a) -> m a
   gcatchJust :: (Exception -> Maybe b) -> m a -> (b -> m a) -> m a
+#endif
+
+gtry a = gcatch (liftM Right a) (return . Left)
+gtryJust p a = gcatchJust p (liftM Right a) (return . Left)
 
 instance CaughtMonadIO IO where
   gcatch = Control.Exception.catch
@@ -48,3 +43,20 @@ instance CaughtMonadIO m => CaughtMonadIO (ReaderT a m) where
          Just e' -> runReaderT (h e') r
        )
 
+gbracket :: (CaughtMonadIO m) =>
+  m t -> (t -> m a) -> (t -> m b) -> m b
+gbracket acq rel act = do
+  r <- acq
+  gcatch (do
+      v <- act r
+      rel r
+      return v
+    )
+#ifdef NEW_EXCEPTION
+    (\e@(SomeException _) -> rel r >> throw e)
+#else
+    (\e -> rel r >> throw e)
+#endif
+
+gfinally :: (CaughtMonadIO m) => m t -> m a -> m t
+gfinally a f = gbracket (return ()) (const f) (const a)
